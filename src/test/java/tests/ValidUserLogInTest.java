fcce2f93c0c4451c29dd4b44be31980a60a41cdb
kostiantyn.sichenko <kostyantins@gmail.com>
2017-07-20 10:42:29 +0300

    Added selenide and webDriver manager

--- src/test/java/tests/ValidUserLogInTest.java
+++ src/test/java/tests/ValidUserLogInTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
+import org.openqa.selenium.By;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import util.TestRunner;
@@ -11,23 +13,33 @@ import static pageobjects.UserAccountPage.user;
 
 public class ValidUserLogInTest extends TestRunner {
 
+//    @Test
+//    public final void testValidUserLogIn() {
+//        logInPage
+//                .setEmail()
+//                .setPassword()
+//                .doAuthorization();
+//
+//        //custom assert
+//        assertThat(user).isLogInAs("Vitaliy Pupkin");
+//
+//        //assert with selenide
+//        $(user).shouldHave(text("Vitaliy Pupkin"));
+//    }
+//
+//    @AfterMethod
+//    public final void logOut() {
+//        logInPage
+//                .doLogOut();
+//    }
+
     @Test
-    public final void testValidUserLogIn() {
-        logInPage
-                .setEmail()
-                .setPassword()
-                .doAuthorization();
+    public void testLogIn() {
+        $(By.id("username")).sendKeys("Igor.Tazylyak");
+        $(By.id("password")).sendKeys("1236456");
+        $(By.id("submit")).click();
 
-        //custom assert
-        assertThat(user).isLogInAs("Vitaliy Pupkin");
 
-        //assert with selenide
-        $(user).shouldHave(text("Vitaliy Pupkin"));
-    }
-
-    @AfterMethod
-    public final void logOut() {
-        logInPage
-                .doLogOut();
+//        $(homePage.getHomePageLocators().getUserLoginText().getText()).shouldHave(Condition.text("Igor.Tazylyak"));
     }
 }
